c947a0ea6801576ea7d99dc2b5d07197677e9558
OngSY97 <sheakyer97@hotmail.com>
2017-11-16 15:01:41 +0800

    Font Size

---
+++ app/src/main/java/my/edu/tar/lab31toolbar/MainActivity.java
@@ -0,0 +1,108 @@
+package my.edu.tar.lab31toolbar;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.DisplayMetrics;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView TextViewMessage;
+    private float size, max=36, min=8;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Linking UI to program
+        TextViewMessage = (TextView)findViewById(R.id.TextViewMessage);
+
+        //TODO get the default font size
+        size = TextViewMessage.getTextSize()/getScreenDensity();
+
+        if(size < min){
+
+        }
+
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);//support toolbar
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }else if (id == R.id.action_increase){
+            //TODO increase font size
+            if (size < max){
+                size ++;
+                TextViewMessage.setTextSize(size);
+            }
+            else{
+                //getApplicationContext = this
+                Toast.makeText(getApplicationContext(), "This is Maximum Size Already",Toast.LENGTH_SHORT).show();
+            }
+        }else if (id == R.id.action_decrease){
+            //TODO decrease font size
+            if (size > min){
+                size --;
+                TextViewMessage.setTextSize(size);
+            }
+            else{
+                Toast.makeText(getApplicationContext(), "This is Minimum Size Already",Toast.LENGTH_SHORT).show();
+            }
+
+        }else if(id == R.id.action_about){
+            //TODO start the About activity
+            Intent intent = new Intent(this, AboutActivity.class);
+            startActivity(intent);
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public float getScreenDensity(){
+        float sizeDensity=0;
+
+        DisplayMetrics metrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+
+        sizeDensity = metrics.density;
+
+        return sizeDensity;
+    }
+}
